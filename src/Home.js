664e86cbe790e2aac40b2d87f9cba3c5d1bd1441
mahbubsk <skmahbubkr787@gmail.com>
2021-06-03 15:39:56 +0600

    fetching data and written error handler

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,10 @@ const Home = () => {
   
   const [blogs, setBlogs] = useState(null)
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
+
+// some style used here.
   const dFlex = {
     display:'flex',
     alignItems: 'center',
@@ -14,33 +17,64 @@ const Home = () => {
   }
 
   const spinner = {
-    backgroundColor:'#2A9876',
-    color:'#fff',
+    color:'#333',
     borderRadius:'5px',
-    padding:'10px'
+    fontSize:'24px',
+    fontWeight:'bold',
+    padding:'10px',
+    border:'3px dotted #2A9876'
   }
 
- 
+  const errorMessage = {
+    color:'#ff2263',
+    fontSize:'32px',
+    fontWeight:'bold'
+  }
+  //some style used here.
+  
+  
+
+//fetching data from http://localhost:8000/blogs using useEffect function. 
   useEffect(() =>{
-    fetch('http://localhost:8000/blogs')
+    setInterval(() =>{
+      fetch('http://localhost:8000/blogs')
       .then((response) =>{
+        if(response.ok) {
+          throw Error("sorry buddy!! couldn't get the data!");
+        }
         return response.json();
       })
       .then((data) => {
         setBlogs(data);
         setIsPending(false);
+        setError(null);
       })
+      .catch((error) =>{
+        setIsPending(false);
+        setError(error.message);
+        console.log(error.message);
+      })
+    }, 2000)
   },[]);
   
-  
+
   return (
       <div className="home">
+        { 
+          error && 
+            <div style={dFlex}>
+              <span style={errorMessage}>{error}</span>
+            </div> 
+        }
         { isPending && 
           <div style={dFlex}>
               <span style={spinner}>loading....</span>
           </div>
         }
-        {blogs && <BlogLists blogs={blogs} title={"All The Blogs Here!!"}/>}
+        {
+          blogs && 
+          <BlogLists blogs={blogs} title={"All The Blogs Here!!"}/>
+        }
       </div>
   );
 }
